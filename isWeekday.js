f720627512142f8597966dc6eef6f660c31ae0d9
terror tivani <terror.tivani@gmail.com>
2022-08-25 11:46:47 +0200

    final

---
+++ isWeekday.js
@@ -0,0 +1,21 @@
+//  const DayName = ["Sunday", 
+//                  "Monday", 
+//                  "Tuesday", 
+//                  "Wednesday",
+//                  "Thursday", 
+//                  "Friday", 
+//                  "Saturday"];
+
+export default function isWeekday(DayName){
+if (DayName.startsWith("M")){
+return true;
+}else if (DayName.startsWith("T")){
+return true;
+}else if (DayName.startsWith("W")){
+return true;
+}else if (DayName.startsWith("F")){
+return true;
+}else{
+return false;
+}
+}
---
+++ test/isWeekday.tests.js
@@ -0,0 +1,23 @@
+import assert from "assert";
+import isWeekday from "../isWeekday.js";
+
+describe ("The Weekday function", function() {
+
+    it ("should  return 'false' when call in 'Sutarday'", function() {
+    
+        assert.equal(isWeekday('Saturday'), false);
+    });
+
+    it ("should  return 'false' when call in 'Sunday'", function() {
+    
+        assert.equal(isWeekday('Sunday'), false);
+    });
+
+    it ("should  return 'true' when call in 'Monday to Friday'", function() {
+    
+        assert.equal(isWeekday('Monday'), true);
+    });
+
+
+
+});
